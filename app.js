a87688034dd977ec590ae15c03675a52dc976e67
Sean Roberts <sean.roberts90@gmail.com>
2015-11-30 07:51:52 -0500

    adding handlebars and fixing local dev setup

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
+var exphbs  = require('express-handlebars');
 var routes = require('./routes/index');
 
 var app = express();
@@ -14,9 +14,14 @@ var app = express();
 // versions of the running app on deployment
 // process.title = 'atlskatesite';
 
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.engine('.hbs', exphbs({
+    defaultLayout: 'main',
+    extname: '.hbs'
+}));
+app.set('view engine', '.hbs');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
