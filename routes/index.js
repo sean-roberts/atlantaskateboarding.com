18da7dea5a9d59cbde6d1eef1256959d3f8208ee
Sean Roberts <sean.roberts90@gmail.com>
2016-09-20 21:28:51 -0400

    save black blocks initial

--- routes/index.js
+++ routes/index.js
@@ -3,10 +3,14 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    res.render('splash', {
+    res.render('blackblocks', {
         title: 'Atlanta Skateboarding - Georgia Skateboarding',
         layout: 'splash'
     });
+    // res.render('splash', {
+    //     title: 'Atlanta Skateboarding - Georgia Skateboarding',
+    //     layout: 'splash'
+    // });
 });
 
 module.exports = router;
